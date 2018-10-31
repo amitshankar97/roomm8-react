2cc87e6b54b3a8d5dbd37459251c8021d4032535
Victor Gomes <gomesvictorluiz@gmail.com>
2018-10-30 18:27:48 -0700

    add the comment to another file

--- src/components/propertyProfile/propertyProfile.js
+++ src/components/propertyProfile/propertyProfile.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import './propertyProfile.css';
 import SampleApartment from './sampleApartment.jpg';
 import SamplePicture from '../profile/sample_profile_picture.jpg';
+import Comments from './comments';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 
 const GOOGLE_API_KEY = "AIzaSyBPsMu2UJW4WKemA-_glrl6Bh5NyS3SIj0";
@@ -50,11 +51,11 @@ class PropertyProfile extends Component {
                                     <label>Rating</label>
                                 </div>
                                 <div className="col-md-6">
-                                    <span class="fa fa-star checked"></span>
-                                    <span class="fa fa-star checked"></span>
-                                    <span class="fa fa-star checked"></span>
-                                    <span class="fa fa-star"></span>
-                                    <span class="fa fa-star"></span>
+                                    <span className="fa fa-star checked"></span>
+                                    <span className="fa fa-star checked"></span>
+                                    <span className="fa fa-star checked"></span>
+                                    <span className="fa fa-star"></span>
+                                    <span className="fa fa-star"></span>
                                 </div>
                             </div>
                             <div className="row">
@@ -81,13 +82,17 @@ class PropertyProfile extends Component {
                                     <p>1 East Speedway Blvd, Tucson, AZ</p>
                                 </div>
                             </div>
-                            <MyMapComponent
-                                isMarkerShown
-                                googleMapURL={"https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=" + GOOGLE_API_KEY}
-                                loadingElement={<div style={{ height: `100%` }} />}
-                                containerElement={<div style={{ height: `300px`, width: `80%`, marginBottom: `15px` }} />}
-                                mapElement={<div style={{ height: `100%` }} />}
-                            />
+                            <div className="row">
+                                <div className="col-md-9">
+                                    <MyMapComponent
+                                        isMarkerShown
+                                        googleMapURL={"https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=" + GOOGLE_API_KEY}
+                                        loadingElement={<div style={{ height: `100%` }} />}
+                                        containerElement={<div style={{ height: `300px`, marginBottom: `15px` }} />}
+                                        mapElement={<div style={{ height: `100%` }} />}
+                                    />
+                                </div>
+                            </div>
                             <div className="row">
                                 <div className="col-md-6">
                                     <label>Parking</label>
@@ -121,48 +126,59 @@ class PropertyProfile extends Component {
                             </div>
                             <br />
                             <div className="row">
-                                <div className="col-md-12">
-                                    <form>
-                                      <div class="form-group">
-                                        <label for="comment">Name:</label>
-                                        <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Name" />
-                                        </div>
-                                        <div class="form-check form-check-inline">
+                                <div className="col-md-9">
+                                    <h4>Comments: </h4>
+                                </div>
+                            </div>
 
-                                            <label for="comment">Rating:</label>
-                                        </div>
-                                        <div class="form-check form-check-inline">
-                                            <label class="form-check-label">
-                                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" /> 1
-                                            </label>
-                                        </div>
-                                        <div class="form-check form-check-inline">
-                                            <label class="form-check-label">
-                                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" /> 2
-                                            </label>
-                                        </div>
-                                        <div class="form-check form-check-inline disabled">
-                                            <label class="form-check-label">
-                                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3" /> 3
-                                            </label>
-                                        </div>
-                                        <div class="form-check form-check-inline disabled">
-                                            <label class="form-check-label">
-                                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio4" value="option4" /> 4
-                                            </label>
+                            <div className="row">
+                                <div className="col-md-9">
+                                    
+                                    <div className="form-group">
+                                    <label htmlFor="comment">Name:</label>
+                                    <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Name" />
+                                    </div>
+                                    <div className="form-check form-check-inline">
 
-                                        </div>
-                                        <div class="form-check form-check-inline disabled">
-                                            <label class="form-check-label">
-                                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio5" value="option5" /> 5
-                                            </label>
-                                        </div>
-                                        <div class="form-group">
-                                        <label for="comment">Comment:</label>
-                                        <textarea class="form-control" rows="3" id="comment"></textarea>
-                                        </div>
+                                        <label htmlFor="comment">Rating:</label>
+                                    </div>
+                                    <div className="form-check form-check-inline">
+                                        <label className="form-check-label">
+                                        <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"/> 1
+                                        </label>
+                                    </div>
+                                    <div className="form-check form-check-inline">
+                                        <label className="form-check-label">
+                                        <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" /> 2
+                                        </label>
+                                    </div>
+                                    <div className="form-check form-check-inline disabled">
+                                        <label className="form-check-label">
+                                        <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3" /> 3
+                                        </label>
+                                    </div>
+                                    <div className="form-check form-check-inline disabled">
+                                        <label className="form-check-label">
+                                        <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio4" value="option4" /> 4
+                                        </label>
+
+                                    </div>
+                                    <div className="form-check form-check-inline disabled">
+                                        <label className="form-check-label">
+                                        <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio5" value="option5" checked/> 5
+                                        </label>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="comment">Comment:</label>
+                                        <textarea className="form-control" rows="3" id="comment"></textarea>
+                                    </div>
                                         
-                                    </form>
+                                    
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-md-9">
+                                    <Comments />
                                 </div>
                             </div>
                         </div>
